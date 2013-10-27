3d88f95253f3c8aadc51f0d73ea19dd0b7389733
Julian Haupt <julian.haupt@spielmeister.com>
2013-10-27 18:50:18 +0100

    use the bundled ant

--- src/spell/shared/build/external/ant.js
+++ src/spell/shared/build/external/ant.js
@@ -4,28 +4,42 @@ define(
 		'spell/shared/build/external/java',
 
 		'fs',
+		'os',
+		'path',
 		'spell/shared/build/spawnChildProcess'
 	],
 	function(
 		java,
 		fs,
+		os,
+		path,
 		spawnChildProcess
 	) {
 		'use strict'
+
+		var getAntPath = function( environmentConfig ) {
+
+			return path.join( environmentConfig.spellCliPath, 'ant', 'bin', os.platform() == "win32" ? 'ant.bat' : 'ant' )
+		}
 
 
 		return {
 
 			checkPrerequisite: function( environmentConfig, successCb, failCb ) {
+				var antPath = getAntPath( environmentConfig )
 
-				//TODO
+				if( !fs.existsSync( antPath ) ) {
+					failCb( 'Could not find ant in ' + antPath )
+					return
+				}
+
 				successCb()
 			},
 
 			run: function( environmentConfig, argv, cwd, next ) {
 				// build the android project
 				spawnChildProcess(
-					'ant',
+					getAntPath( environmentConfig ),
 					argv,
 					java.getProcessEnv( environmentConfig, cwd ),
 					true,
