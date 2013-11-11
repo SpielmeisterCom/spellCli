0a30385985c97496d1584d0f0dd3c13c5f9a6f31
Julian Haupt <jh@spielmeister.com>
2013-11-11 23:50:00 +0100

    further work on iOS builder

--- src/spell/shared/build/target/ios/iOSBuilder.js
+++ src/spell/shared/build/target/ios/iOSBuilder.js
@@ -52,71 +52,107 @@ define(
 	)
 	{
 		var build = function( environmentConfig, projectPath, projectLibraryPath, outputPath, target, projectConfig, library, cacheContent, scriptSource, minify, anonymizeModuleIds, debug, next ) {
-			var spellCorePath           = environmentConfig.spellCorePath,
-				spellAndroidPath        = environmentConfig.spelliOSPath,
-				iOSBuildSettings        = projectConfig.config.ios || {}
+            var projectId               = projectConfig.config.projectId || 'defaultProjectId',
+                spellCorePath           = environmentConfig.spellCorePath,
+				spelliOSPath            = environmentConfig.spelliOSPath || '../spelliOS/build',
+                tealeafPath             = path.resolve( spelliOSPath, debug ? 'debug' : 'release', 'tealeaf' ),
+                iOSBuildSettings        = projectConfig.config.ios || {},
+                iOSBundleId             = iOSBuildSettings.bundleId ? iOSBuildSettings.bundleId : 'com.spelljs.' + projectId,
+                tmpProjectPath          = path.join( projectPath, 'build', 'tmp', 'iOS', projectId ),
+                resourcesPath           = path.join( tmpProjectPath, 'assets', 'resources' ),
+                spellEngineFile         = createDebugPath( debug, 'spell.debug.js', 'spell.release.js', path.join( spellCorePath, 'lib' ))
+
+            console.log( '[spellcli] Cleaning ' + tmpProjectPath )
+            emptyDirectory( tmpProjectPath )
+
+            var f = ff(
+                function() {
+                    //Set timeout for prerequisite check to 5s
+                    f.timeout( 5000 )
+                },
+                function() {
+                    console.log( '[spellcli] Checking prerequisite: spellCore build' )
+
+                    if( !fs.existsSync( spellEngineFile ) ) {
+                        f.fail( 'Could not find a spellCore build in ' + spellEngineFile )
+                    }
+                },
+                function() {
+                    console.log( '[spellcli] Checking prerequisite: xcodebuild' )
+                    xcodebuild.checkPrerequisite( environmentConfig, f.wait(), f.fail )
+                },
+                function() {
+                    console.log( '[spellcli] Checking prerequisite: xcrun' )
+                    xcrun.checkPrerequisite( environmentConfig, f.wait(), f.fail )
+                },
+                function() {
+                    console.log( '[spellcli] Checking prerequisite: installed iphone sdk' )
+                    xcodebuild.getInstalledSdks( environmentConfig, f.slotPlain(), f.fail )
+                },
+                function( SDKs ) {
+                    //TODO: check if iphoneos6.1 sdk is installed
+
+                    console.log( SDKs )
+                },
+                function() {
+                    //Set timeout to 5 min
+                    f.timeout( 5 * 60 * 1000 )
+                },
+                function() {
+                    // copy the prebuild Tealeaf library into our temp directory
+                    wrench.copyDirSyncRecursive(
+                        tealeafPath,
+                        tmpProjectPath,
+                        {
+                            forceDelete : true,
+                            preserveFiles : false,
+                            inflateSymlinks : false
+                        }
+                    )
+                },
+                function () {
+                    var params = [
+                        '-target',
+                        iOSBundleId,
+
+                        '-sdk',
+                        'iphoneos6.1',
+
+                        '-configuration',
+                        debug ? 'Debug' : 'Release',
+
+                        '-jobs',
+                        8
+                    ]
+
+                    xcodebuild.run(
+                        environmentConfig,
+                        params,
+                        tmpProjectPath,
+                        f.wait()
+                    )
+
+                }
 
 
-			var f = ff(
-				function() {
-					//Set timeout for prerequisite check to 5s
-					f.timeout( 5000 )
-				},
-				function() {
-					console.log( '[spellcli] Checking prerequisite: xcodebuild' )
-					xcodebuild.checkPrerequisite( environmentConfig, f.wait(), f.fail )
-				},
-				function() {
-					console.log( '[spellcli] Checking prerequisite: xcrun' )
-					xcrun.checkPrerequisite( environmentConfig, f.wait(), f.fail )
-				},
-				function() {
-					console.log( '[spellcli] Checking prerequisite: installed iphone sdk' )
-					xcodebuild.getInstalledSdks( environmentConfig, f.slot(), f.fail )
-				},
-				function( SDKs ) {
-					console.log( SDKs )
-				},
+                //sign app
+            /*function () {
+                    var args = [
+                     '-sdk', 'iphoneos',
+                     'PackageApplication',
+                     '-v',
+                     path.resolve(path.join(projectPath, 'build/'+configurationName+'-iphoneos/'+appName+'.app')),
+                     '-o',
+                     path.resolve(outputIPAPath),
+                     '--sign',
+                     'iPhone Developer: ' + developerName,
+                     '--embed',
+                     path.resolve(provisionPath)
+                     ]
 
-				//build app
-				function () {
-					var configurationName = debug ? 'Debug' : 'Release',
-						sdk = 'iphoneos6.1'
+                    //xcrun.run( environmentConfig, params, '', f.wait() )
 
-					var params = [
-						'-target',
-						iOSBuildSettings.bundleID,
-						'-sdk',
-						sdk,
-						'-configuration',
-						configurationName,
-						'-jobs',
-						8
-					]
-
-					//xcodebuild.run( environmentConfig, params, '', f.wait() )
-
-				},
-
-
-				//sign app
-				function () {
-					/*var args = [
-						'-sdk', 'iphoneos',
-						'PackageApplication',
-						'-v',
-						path.resolve(path.join(projectPath, 'build/'+configurationName+'-iphoneos/'+appName+'.app')),
-						'-o',
-						path.resolve(outputIPAPath),
-						'--sign',
-						'iPhone Developer: ' + developerName,
-						'--embed',
-						path.resolve(provisionPath)
-					]*/
-
-					//xcrun.run( environmentConfig, params, '', f.wait() )
-
-				}
+                }*/
 
 			).onError( function( message ) {
 				console.log( message )
