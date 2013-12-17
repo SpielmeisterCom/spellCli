20a1d28e480dab52a64a8e6a8bd01d542e41d562
Ioannis Kostomanolakis <ik@spielmeister.com>
2013-12-17 17:20:28 +0100

    Windows Phone 8 WmAppManifest.xml generation & icon mapping

--- src/spell/cli/build/target/windows/WindowsPhoneBuilder.js
+++ src/spell/cli/build/target/windows/WindowsPhoneBuilder.js
@@ -11,7 +11,6 @@ define(
 		'spell/cli/build/target/web/WebBuilder',
 
 		'spell/cli/build/external/windows/msBuild',
-		'spell/cli/build/external/windows/signing',
 
 		'ff',
 		'fs',
@@ -31,7 +30,6 @@ define(
 		WebBuilder,
 
 		msBuild,
-		signing,
 
 		ff,
 		fs,
@@ -54,20 +52,21 @@ define(
 		}
 
 		var build = function( environmentConfig, projectPath, projectLibraryPath, outputPath, target, projectConfig, library, cacheContent, scriptSource, minify, anonymizeModuleIds, debug, next ) {
-			var projectId           = projectConfig.config.projectId || 'defaultProjectId',
-				tmpProjectPath      = path.join( projectPath, 'build', 'tmp', 'winphone'),
-				outputPath          = path.join( outputPath, 'winphone'),
-				skeletonProjectPath = environmentConfig.spellWindowsPhone8,
-				buildTarget         = debug ? 'Debug' : 'Release'
+			var projectId             = projectConfig.config.projectId || 'defaultProjectId',
+				tmpProjectPath        = path.join( projectPath, 'build', 'tmp', 'winphone'),
+				outputPath            = path.join( outputPath, 'winphone'),
+				skeletonProjectPath   = environmentConfig.spellWindowsPhone8,
+				buildTarget           = debug ? 'Debug' : 'Release',
+				winphoneBuildSettings = projectConfig.config.winphone,
+				displayName           = winphoneBuildSettings.displayName,
+				iconPath              = "Assets\\ApplicationIcon.png",
+				smallIcon             = "Assets\\Tiles\\FlipCycleTileSmall.png",
+				mediumIcon            = "Assets\\Tiles\\FlipCycleTileMedium.png"
 
 			var f = ff(
 				function() {
 					console.log( '[spellcli] Checking prerequisite: MSBuild.exe' )
 					msBuild.checkPrerequisite( environmentConfig, f.wait(), f.fail )
-				},
-				function() {
-					console.log( '[spellcli] Checking prerequisite: SignTool.exe' )
-					signing.checkPrerequisite( environmentConfig, f.wait(), f.fail )
 				},
 				function() {
 					console.log( '[spellcli] Cleaning ' + tmpProjectPath )
@@ -170,6 +169,81 @@ define(
 						xamlFilePath,
 						fileContent
 					)
+				},
+				function() {
+					console.log( '[spellcli] writing WMAppManifest file' )
+
+					var version              = projectConfig.config.version || '1.0.0.0',
+						language             = projectConfig.config.defaultLanguage || "en-us",
+						description          = winphoneBuildSettings.description || 'A Windows Store App created with SpellJS',
+						author               = winphoneBuildSettings.author,
+						publisher            = winphoneBuildSettings.publisher
+
+					var root = xmlbuilder.create()
+
+					var node = root.ele( 'Deployment', { xmlns: 'http://schemas.microsoft.com/windowsphone/2012/deployment', AppPlatformVersion: "8.0"})
+						.ele( 'DefaultLanguage', { xmlns: "", code: language } ).up().ele( 'Languages', { xmlns: "" } )
+
+					_.each(
+						projectConfig.config.supportedLanguages,
+						function( supportedLanguage ) {	node.ele( "Language", { code: supportedLanguage} ) }
+					)
+
+					node.up()
+						.ele( 'App',{
+							xmlns: "",
+							ProductID: "{9cc34fff-f4f8-44ad-9d67-367c2c2a0936}",
+							Title: displayName,
+							RuntimeType: "Silverlight",
+							Version: version,
+							Genre: "apps.normal",
+							Author: author,
+							Description: description,
+							Publisher: publisher,
+							PublisherID: "{123f91e3-8000-4af5-9465-bfb807d8fc9b}"
+						})
+						.ele( 'IconPath', { IsRelative: true, IsResource: false } ).txt( iconPath ).up()
+						.ele( 'Capabilities' )
+							.ele( 'Capability', { Name: 'ID_CAP_NETWORKING' }).up()
+							.ele( 'Capability', { Name: 'ID_CAP_SENSORS' }).up()
+							.ele( 'Capability', { Name: 'ID_CAP_WEBBROWSERCOMPONENT' }).up()
+						.up()
+						.ele( 'Tasks' )
+							.ele( 'DefaultTask', { Name: "_default", NavigationPage: "MainPage.xaml" }).up().up()
+						.ele( 'Tokens' )
+							.ele( 'PrimaryToken', { TokenID: displayName + 'Token', TaskName: "_default" })
+								.ele( 'TemplateFlip' )
+									.ele( 'SmallImageURI', { IsRelative: true, IsResource: false } ).txt( smallIcon ).up()
+									.ele( 'Count').txt(0).up()
+									.ele( 'BackgroundImageURI', { IsRelative: true, IsResource: false } ).txt( mediumIcon ).up()
+									.ele( 'Title').txt(displayName).up()
+									.ele( 'BackContent').up()
+									.ele( 'BackBackgroundImageURI').up()
+									.ele( 'BackTitle').up()
+									.ele( 'DeviceLockImageURI').up()
+									.ele( 'HasLarge').up()
+								.up()
+							.up()
+						.up()
+						.ele( 'ScreenResolutions' )
+							.ele( 'ScreenResolution', { Name: 'ID_RESOLUTION_WVGA' }).up()
+							.ele( 'ScreenResolution', { Name: 'ID_RESOLUTION_WXGA' } ).up()
+							.ele( 'ScreenResolution', { Name: 'ID_RESOLUTION_HD720P' } ).up()
+
+					var xmlContent = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+					xmlContent += root.toString( { pretty : true } )
+
+					writeFile( path.join( tmpProjectPath, 'SpellJSProjectSceleton', 'Properties', 'WMAppManifest.xml'), xmlContent )
+				},
+				function() {
+					console.log( '[spellcli] Copy images' )
+
+					var resourcesPath = path.join( projectPath, 'resources', 'winphone'),
+						assetsPath    = path.join( tmpProjectPath, 'SpellJSProjectSceleton' )
+
+					fsUtil.copyFile( path.join( resourcesPath, 'ApplicationIcon.png' ) , path.join( assetsPath, iconPath ) )
+					fsUtil.copyFile( path.join( resourcesPath, 'FlipCycleTileSmall.png' ) , path.join( assetsPath, smallIcon ) )
+					fsUtil.copyFile( path.join( resourcesPath, 'FlipCycleTileMedium.png' ) , path.join( assetsPath, mediumIcon ) )
 				},
 				function() {
 					var cwd = path.join( tmpProjectPath, 'SpellJSProjectSceleton.sln' )
